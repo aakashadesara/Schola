b5d1825c0b52a84b2642b5228eb0df3a3c1e457a
Aakash Adesara <aakash.adesara@gmail.com>
2015-10-25 07:20:11 -0400

    THIS IS THE SAVE BRO

---
+++ js/prompts.js
@@ -0,0 +1,36 @@
+var nostalgiaQuotes = ["We are made wise not by the recollection of our past, but by the responsibility for our future.",
+						"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+						"A people without the knowledge of their past history, origin and culture is like a tree without roots.",
+						"Look back, and smile on perils past.",
+						"Gratitude makes sense of our past, brings peace for today, and creates a vision for tomorrow.",
+						"The past cannot be changed. The future is yet in your power."];
+
+var gpaPrompts = [["Your choice college, ", " has a median GPA of ", " so we recommend that you maintain a ", " for your remaining years in high school."],
+				  ["", " has generally accepted applicants with a GPA of ", " so it is recommended that you keep your grades at at least a ", " for the rest of high school."],
+				  ["One of your indicated choices, ", " tends to accept students with ", " and so it is recommended that you try to maintain a ", " for the rest of your time in high school."]];
+
+var SATprompts = [["Your SAT score was below what ", " generally admits. You may want to retake again in ", " and attempt to score a "],
+				  ["We understand that getting up on a Saturday morning for an SAT is a hassle but you scored just a tad below what ", "'s pool of admittees SAT scores were. We suggest retaking in ", " to score above a "],
+				  ["Not bad not bad. You can definetley do better on the SAT though! ", " chooses applicants with high calliber SAT scores and your score barely missed that mark. Take it again in ", " and try scoring at least a "]];
+
+var ACTprompts = [["Your ACT score was below what ", " generally admits. You may want to retake again in ", " and attempt to score a "],
+					["We understand that getting up on a Saturday morning for an ACT is a hassle but you scored just a tad below what ", "'s pool of admittees ACT scores were. We suggest retaking in ", " to score above a "],
+					["Not bad not bad. You can definetley do better on the ACT though! ", " chooses applicants with high calliber ACT scores and your score barely missed that mark. Take it again in ", " and try scoring at least a "]];
+
+var ecPromptsStart = [["Considering that you would like to major in a field related to ", ", we suggest that you look into getting involved with an organization that focuses on ", ", or "],
+					  ["", " is a vast field and there are many opportunities available to high schoolers to get involved in this subject early on. Look up ", " and "],
+					  ["Colleges love seeing interest in subject fields. Since you want to pursue ", " we think you should look into getting involved with organizations that deal with ", " and "],
+					  ["People who study ", " often times participate in ", " and "],
+					  ["Considering that you would like to major in a field related to ", ", we suggest that you look into getting involved with an organization that focuses on ", ", or "],
+					  ["", " is a vast field and there are many opportunities available to high schoolers to get involved in this subject early on. Look up ", " and "],
+					  ["Colleges love seeing interest in subject fields. Since you want to pursue ", " we think you should look into getting involved with organizations that deal with ", " and "],
+					  ["People who study ", " often times participate in ", " and "],
+					  ["Considering that you would like to major in a field related to ", ", we suggest that you look into getting involved with an organization that focuses on ", ", or "],
+					  ["", " is a vast field and there are many opportunities available to high schoolers to get involved in this subject early on. Look up ", " and "],
+					  ["Colleges love seeing interest in subject fields. Since you want to pursue ", " we think you should look into getting involved with organizations that deal with ", " and "],
+					  ["People who study ", " often times participate in ", " and "]];
+
+var byebyePrompts = [["It was a good run ", " but the show must go on. Say bye to high school."],
+ 					 ["Say adios to ", " since you are graduating!"],
+ 					 ["Congratulations! You will have graduated from ", " by this time."],
+ 					 ["Strive for the golden light at the end of the road. You are almost done and out of ", ""]];
--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
+var user;
+
 $(document).ready(function(){
 
-	var user = [];
+	
+	document.onkeydown = function() {
+		var audio = new Audio('assets/type.wav');
+		audio.play();
+	}
+
+	user = [];
 	var collegeList = [];
 	var subjectTests = [];
 
@@ -16,6 +24,11 @@ $(document).ready(function(){
 		$("#1stSection").fadeOut(500);
 		$('#infoSection1').show();
 		$("#infoSection1").addClass('animated bounceInUp');
+
+		for(var i = 0; i < majorList.length; i++){
+			$("#majorHolder").html($("#majorHolder").html() + "<option>" + majorNames[i] + "</option>");
+		}
+
 	});
 
 	$("#initButton2").click(function(){
@@ -88,30 +101,31 @@ $(document).ready(function(){
 		user.push($("#actReadingHolder").val());	//11
 		user.push($("#actScienceHolder").val());	//12
 
-		user.push($("#ec1").val());					//13
-		user.push($("#ec2").val());					//14
-		user.push($("#ec3").val());					//15
-		user.push($("#ec4").val());					//16
-		user.push($("#ec5").val());					//17
-		user.push($("#ec6").val());					//18
-		user.push($("#ec7").val());					//19
-		user.push($("#ec8").val());					//20
-		user.push($("#ec9").val());					//21
-		user.push($("#ec10").val());				//22
-		user.push($("#ec11").val());				//23
-		user.push($("#ec12").val());				//24
+		user.push($("#ecMini1").val() + " " + $("#ec1").val());					//13
+		user.push($("#ecMini2").val() + " " + $("#ec2").val());					//14
+		user.push($("#ecMini3").val() + " " + $("#ec3").val());					//15
+		user.push($("#ecMini4").val() + " " + $("#ec4").val());					//16
+		user.push($("#ecMini5").val() + " " + $("#ec5").val());					//17
+		user.push($("#ecMini6").val() + " " + $("#ec6").val());					//18
+		user.push($("#ecMini7").val() + " " + $("#ec7").val());					//19
+		user.push($("#ecMini8").val() + " " + $("#ec8").val());					//20
+		user.push($("#ecMini9").val() + " " + $("#ec9").val());					//21
+		user.push($("#ecMini10").val() + " " + $("#ec10").val());				//22
+		user.push($("#ecMini11").val() + " " + $("#ec11").val());				//23
+		user.push($("#ecMini12").val() + " " + $("#ec12").val());				//24
 
 		user.push(subjectTests); 					//25
 
 		user.push(collegeList);						//26
 		user.push($("#totalClassesHolder").val());	//27
 
-		console.log(user);
+		//console.log(user);
 
 		$('#infoSection4').addClass('animated bounceOutUp');
 		$("#infoSection4").fadeOut(500);
 		$("#informationDiv").show();
 
+		generateTimeline();
 
 		var timelineGPA = gpaNeededToMeet(highestGPAFromColleges(user[26]), user[27], calculateYear(user[3]), user[6]);
 		var timelineSAT = takeAgainSAT(computeUserSAT(user[7], user[8]), highestSATFromcCollege(user[26]));
@@ -122,12 +136,7 @@ $(document).ready(function(){
 
 		//$("#informationDiv").html("First Name: " + user[0] + "\n" + "GPA " + user[5] + "\nW GPA " + user[6] + "GPA Needed " + timelineGPA);
 
-		$(".timeline-item").hover(function () {
-		    $(".timeline-item").removeClass("active");
-		    $(this).toggleClass("active");
-		    $(this).prev(".timeline-item").toggleClass("close");
-		    $(this).next(".timeline-item").toggleClass("close");
-		});
+		
 
 	});
 
@@ -148,7 +157,7 @@ $(document).ready(function(){
 
 	// Access college list
 	/*for(var i = 0; i < list.length; i++){
-		console.log(list[i]);
+		//console.log(list[i]);
 	}*/
 });
 
@@ -160,7 +169,7 @@ function setUpSubjectTestCheckboxes(){
 
 function addECSlots(){
 	for(var i = 0; i < 12; i++){
-		$("#ecHolders").html($("#ecHolders").html() + "<div class=\"panel panel-primary col-md-3\">            <div class=\"panel-heading\">              <h3 class=\"panel-title\">                <div class=\"form-group\">                <input  style=\"background-color: rgba(0,0,0,0); color:white; outline: none; border: 0;\" type=\"text\" placeholder=\"Activity " + (i+1) + " Title...\">              </div>            </h3>            </div>            <div class=\"panel-body\">              <textarea  style=\"background-color: rgba(0,0,0,0); color:black; outline: none; border: 0;\" type=\"text\" id=\"ec" + (i+1) + "\" placeholder=\"Activity " + (i+1) + " Description...\"></textarea>            </div>        </div>");
+		$("#ecHolders").html($("#ecHolders").html() + "<div class=\"panel panel-primary col-md-3\">            <div class=\"panel-heading\">              <h3 class=\"panel-title\">                <div class=\"form-group\">                <input  id=\"ecMini" + (i+1) + "\" style=\"background-color: rgba(0,0,0,0); color:white; outline: none; border: 0;\" type=\"text\" placeholder=\"Activity " + (i+1) + " Title...\">              </div>            </h3>            </div>            <div class=\"panel-body\">              <textarea  style=\"background-color: rgba(0,0,0,0); color:black; outline: none; border: 0;\" type=\"text\" id=\"ec" + (i+1) + "\" placeholder=\"Activity " + (i+1) + " Description...\"></textarea>            </div>        </div>");
 	}
 }
 
@@ -172,19 +181,39 @@ function calculateYear(num){
 }
 
 function gpaNeededToMeet(gpaMAX, numClasses, year, kidGPA){
-	console.log(year + " ASDASDAS")
+	//console.log(year + " ASDASDAS")
 	var gpNeeded = gpaMAX * numClasses/year * 3;
 	var gpKid = kidGPA * numClasses;
 	return (gpNeeded - gpKid) / (numClasses/year * 3 - numClasses);
 }
 
+function highestGPAFromCollegesName(arr){
+	var highestGPA = 0;
+	var z = 0;
+	for(var i = 0; i < arr.length; i++){
+		//console.log(arr[i]);
+		for(var j = 0; j < list.length; j++){
+			if(arr[i] === list[j][0]){
+				//console.log(list[j][0]);
+				if(highestGPA < list[j][6]){
+					highestGPA = list[j][6];
+					z = j;
+				}
+			}
+		}
+	}
+	return list[z][0];
+}
+
+
+
 function highestGPAFromColleges(arr){
 	var highestGPA = 0;
 	for(var i = 0; i < arr.length; i++){
-		console.log(arr[i]);
+		//console.log(arr[i]);
 		for(var j = 0; j < list.length; j++){
 			if(arr[i] === list[j][0]){
-				console.log(list[j][0]);
+				//console.log(list[j][0]);
 				if(highestGPA < list[j][6]){
 					highestGPA = list[j][6];
 				}
@@ -197,10 +226,10 @@ function highestGPAFromColleges(arr){
 function highestSATFromcCollege(arr){
 	var highestSAT = 0;
 	for(var i = 0; i < arr.length; i++){
-		console.log(arr[i]);
+		//console.log(arr[i]);
 		for(var j = 0; j < list.length; j++){
 			if(arr[i] === list[j][0]){
-				console.log(list[j][0]);
+				//console.log(list[j][0]);
 				if(highestSAT < list[j][7]){
 					highestSAT = list[j][7];
 				}
@@ -211,7 +240,7 @@ function highestSATFromcCollege(arr){
 }
 
 function computeUserSAT(m, r){
-	return m + r;
+	return parseInt(m) + parseInt(r);
 }
 
 function takeAgainSAT(score, highest){
@@ -221,10 +250,10 @@ function takeAgainSAT(score, highest){
 function highestACTFromcCollege(arr){
 	var highestACT = 0;
 	for(var i = 0; i < arr.length; i++){
-		console.log(arr[i]);
+		//console.log(arr[i]);
 		for(var j = 0; j < list.length; j++){
 			if(arr[i] === list[j][0]){
-				console.log(list[j][0]);
+				//console.log(list[j][0]);
 				if(highestACT < list[j][8]){
 					highestACT = list[j][8];
 				}
@@ -235,10 +264,247 @@ function highestACTFromcCollege(arr){
 }
 
 function computeUserACT(m, r, s, e){
-	return Math.round((m + r + s + e)/4);
+	return Math.floor((parseInt(m) + parseInt(r) + parseInt(s) + parseInt(e))/4);
 }
 
 function takeAgainACT(score, highest){
 	return !(score >= highest);
 }
+
+var ecArray = [];
+
+function makeECArray(){
+	ecArray.push(user[13]);
+	ecArray.push(user[14]);
+	ecArray.push(user[15]);
+	ecArray.push(user[16]);
+	ecArray.push(user[17]);
+	ecArray.push(user[18]);
+	ecArray.push(user[19]);
+	ecArray.push(user[20]);
+	ecArray.push(user[21]);
+	ecArray.push(user[22]);
+	ecArray.push(user[23]);
+	ecArray.push(user[24]);
+}
+
+/* TimeLine Generator */
+
+
+function generateTimeline(){
+
+	$("#getNameHolder").html(user[0] + " " + user[1]);
+
+	console.log(user);
+	//alert("Current year: " + calculateYear(user[3]));
+	
+	var totalEC = 0;
+	for(var i = 13; i < 25; i++)
+		totalEC += extraCurricularCalculator(user[i]);
+	
+	alert(totalEC);
+
+	makeFreshman();
+	makeSophomore();
+	makeJunior();
+	makeSenior();
+	
+	
+		
+	$(".timeline-item").hover(function () {
+		    $(".timeline-item").removeClass("active");
+		    $(this).toggleClass("active");
+		    $(this).prev(".timeline-item").toggleClass("close");
+		    $(this).next(".timeline-item").toggleClass("close");
+		});
+}
+
+function ecStringGenerator(str){
+	for(var i = 0; i < majorNames.length; i++){
+		if(str === majorList[i][0]){
+			var r = Math.floor(Math.random() * ecPromptsStart.length);
+			
+			return ecPromptsStart[r][0] + user[4] + ecPromptsStart[r][1] + majorList[i][1][Math.floor(Math.random() * majorList[i][1].length)] + ", "+ majorList[i][1][Math.floor(Math.random() * majorList[i][1].length)] + ecPromptsStart[r][2] +  majorList[i][1][Math.floor(Math.random() * majorList[i][1].length)];
+		}
+	}
+}
+
+function makeFreshman(){
+	var nameOfHighestCollege = highestGPAFromCollegesName(user[26]);
+	var highestGPA = highestGPAFromColleges(user[26]);
+	var gpaNeeded = gpaNeededToMeet(highestGPAFromColleges(user[26]), user[27], calculateYear(user[3]), user[6]);
+
+	var randInt1 = Math.floor(Math.random() * gpaPrompts.length);
+	for(var i = 0; i < 10; i++){
+		if(randInt1 < 0 || randInt1 >= gpaPrompts.length){
+			randInt1 = Math.floor(Math.random() * gpaPrompts.length);
+		}
+	}
+	var str1 = gpaPrompts[randInt1][0] + nameOfHighestCollege + gpaPrompts[randInt1][1] + highestGPA + gpaPrompts[randInt1][2] + (Math.floor(100*gpaNeeded)/100) +  gpaPrompts[randInt1][3]; 
+	
+	if(calculateYear(user[3]) <= 1) 
+		$("#freshmanLine").html($("#freshmanLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-danger\">Academics</span>" + str1 + "</div>    </div>");
+
+	$("#freshmanLine").html($("#freshmanLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-warning\">Extracurricular</span>" + ecStringGenerator(user[4]) + "</div>    </div>");
+
+	if(calculateYear(user[3]) > 3) {
+			$("#juniorLine").html($("#juniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"badge\" style=\"color:blue\"></span>" + nostalgiaQuotes[Math.floor(Math.random() * nostalgiaQuotes.length)] + "</div>    </div>");
+
+	}
+
+}
+
+function makeSophomore(){
+	var nameOfHighestCollege = highestGPAFromCollegesName(user[26]);
+	var highestGPA = highestGPAFromColleges(user[26]);
+	var gpaNeeded = gpaNeededToMeet(highestGPAFromColleges(user[26]), user[27], calculateYear(user[3]), user[6]);
+
+	var randInt1 = Math.floor(Math.random() * gpaPrompts.length);
+	for(var i = 0; i < 10; i++){
+		if(randInt1 < 0 || randInt1 >= gpaPrompts.length){
+			randInt1 = Math.floor(Math.random() * gpaPrompts.length);
+		}
+	}
+	var str1 = gpaPrompts[randInt1][0] + nameOfHighestCollege + gpaPrompts[randInt1][1] + highestGPA + gpaPrompts[randInt1][2] + (Math.floor(100*gpaNeeded)/100) +  gpaPrompts[randInt1][3]; 
+	
+	if(calculateYear(user[3]) <= 2) {
+		$("#sophomoreLine").html($("#sophomoreLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-danger\">Academics</span>" + str1 + "</div>    </div>");
+	}
+
+	if(calculateYear(user[3]) > 3) {
+			$("#juniorLine").html($("#juniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"badge\" style=\"color:blue\"></span>" + nostalgiaQuotes[Math.floor(Math.random() * nostalgiaQuotes.length)] + "</div>    </div>");
+
+	}
+
+	if(user[7] == "" || user[7] == "" || user[8] == "" || user[8] == "" || user[9] == "" || user[10] == "" || user[11] == "" || user[12] == ""){
+		$("#sophomoreLine").html($("#sophomoreLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-danger\">Academics</span>" + "Consider enrolling in a test prep class. The SAT and ACT are the two tests colleges look at when making acceptances. Take a diagnostic from both and see which you prefer then study hard for it." + "</div>    </div>");
+	}
+
+	$("#sophomoreLine").html($("#sophomoreLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-warning\">Extracurricular</span>" + ecStringGenerator(user[4]) + "</div>    </div>");
+
+	
+}
+
+function makeJunior(){
+	var nameOfHighestCollege = highestGPAFromCollegesName(user[26]);
+	var highestGPA = highestGPAFromColleges(user[26]);
+	var gpaNeeded = gpaNeededToMeet(highestGPAFromColleges(user[26]), user[27], calculateYear(user[3]), user[6]);
+
+	var randInt1 = Math.floor(Math.random() * gpaPrompts.length);
+	var str1 = gpaPrompts[randInt1][0] + nameOfHighestCollege + gpaPrompts[randInt1][1] + highestGPA + gpaPrompts[randInt1][2] + (Math.floor(100*gpaNeeded)/100) +  gpaPrompts[randInt1][3]; 
+	
+	$("#juniorLine").html($("#juniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-danger\">Academics</span>" + str1 + "</div>    </div>");
+
+	console.log(user[7] + " " + user[8] + " " + highestSATFromcCollege(user[26]) + " " + takeAgainSAT(computeUserSAT(user[7], user[8]), highestSATFromcCollege(user[26])));
+
+	if(takeAgainSAT(computeUserACT(user[9], user[10], user[11], user[12]), highestSATFromcCollege(user[26]))){
+		var randCollege = user[26][Math.floor(Math.random() * user[26].length)];
+		var randInt1 = Math.floor(Math.random() * SATprompts.length);
+		var randDate = "November 1st";
+		var str2 = SATprompts[randInt1][0] + randCollege + SATprompts[randInt1][1] + randDate + SATprompts[randInt1][2] + highestSATFromcCollege(user[26]);
+
+		$("#juniorLine").html($("#juniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-info\">Testing</span>" + str2 + "</div>    </div>");
+	}
+
+	if(takeAgainACT(computeUserACT(user[9], user[10], user[11], user[12]), highestACTFromcCollege(user[26]))){
+		var randCollege = user[26][Math.floor(Math.random() * user[26].length)];
+		var randInt1 = Math.floor(Math.random() * ACTprompts.length);
+		var randDate = "November 1st";
+		var str2 = ACTprompts[randInt1][0] + randCollege + ACTprompts[randInt1][1] + randDate + ACTprompts[randInt1][2] + highestACTFromcCollege(user[26]);
+
+		$("#juniorLine").html($("#juniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-info\">Testing</span>" + str2 + "</div>    </div>");
+	}
+
+	if(calculateYear(user[3]) > 3) {
+			$("#juniorLine").html($("#juniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"badge\" style=\"color:blue\"></span>" + nostalgiaQuotes[Math.floor(Math.random() * nostalgiaQuotes.length)] + "</div>    </div>");
+
+	}
+
+	$("#juniorLine").html($("#juniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-warning\">Extracurricular</span>" + ecStringGenerator(user[4]) + "</div>    </div>");
+
+}
+
+function makeSenior(){
+
+	var nameOfHighestCollege = highestGPAFromCollegesName(user[26]);
+	var highestGPA = highestGPAFromColleges(user[26]);
+	var gpaNeeded = gpaNeededToMeet(highestGPAFromColleges(user[26]), user[27], calculateYear(user[3]), user[6]);
+
+	var randInt1 = Math.floor(Math.random() * gpaPrompts.length);
+	var str1 = gpaPrompts[randInt1][0] + nameOfHighestCollege + gpaPrompts[randInt1][1] + highestGPA + gpaPrompts[randInt1][2] + (Math.floor(100*gpaNeeded)/100) +  gpaPrompts[randInt1][3]; 
+	
+	$("#seniorLine").html($("#seniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-danger\">Academics</span>" + str1 + "</div>    </div>");
+
+	if(takeAgainSAT(computeUserACT(user[9], user[10], user[11], user[12]), highestSATFromcCollege(user[26]))){
+		var randCollege = user[26][Math.floor(Math.random() * user[26].length-1)];
+		var randInt1 = Math.floor(Math.random() * SATprompts.length);
+		var randDate = "November 1st";
+		var str2 = SATprompts[0][0] + randCollege + SATprompts[0][1] + randDate + SATprompts[0][2] + highestSATFromcCollege(user[26]);
+
+		$("#seniorLine").html($("#seniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-info\">Testing</span>" + str2 + "</div>    </div>");
+	}
+
+	if(takeAgainACT(computeUserACT(user[9], user[10], user[11], user[12]), highestACTFromcCollege(user[26]))){
+		var randCollege = user[26][Math.floor(Math.random() * user[26].length)];
+		var randInt1 = Math.floor(Math.random() * ACTprompts.length);
+		var randDate = "November 1st";
+		var str2 = ACTprompts[randInt1][0] + randCollege + ACTprompts[randInt1][1] + randDate + ACTprompts[randInt1][2] + highestACTFromcCollege(user[26]);
+
+		$("#seniorLine").html($("#seniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-info\">Testing</span>" + str2 + "</div>    </div>");
+	}
+
+	$("#seniorLine").html($("#seniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-warning\">Extracurricular</span>" + ecStringGenerator(user[4]) + "</div>    </div>");
+
+	var r2 = Math.floor(Math.random() * byebyePrompts.length);
+	$("#seniorLine").html($("#seniorLine").html() + " <div class=\"timeline-item \">        <div class=\"year\"><span class=\"marker\"><span class=\"dot\"></span></span>        </div>        <div class=\"info\"><span class=\"label label-success\">General</span>" + byebyePrompts[r2][0] + user[2] + byebyePrompts[r2][1] +  "</div>    </div>");
+
+}
+
+
+
+function extraCurricularCalculator(str){
+	
+	
+		str = str.toLowerCase();
+
+		var ecScore = 0;
+		var specificCounter = 0;
+		var generalCounter = 0;
+
+		var wordArr = str.split(" ");
+
+		for(var i = 0; i < majorList.length; i++){
+			if(majorList[i][0] === user[4]){
+				for(var j = 0; j < majorList[i][1].length; j++){
+					for(var z = 0; z < wordArr.length; z++){
+						if(wordArr[z].toLowerCase() === majorList[i][1][j].toLowerCase()){
+							specificCounter++;
+							ecScore += 20 / specificCounter;
+						}
+					}
+				}
+			}
+		}
+
+		for(var i = 0; i < wordArr.length; i++){
+			for(var j = 0; j < generalList.length; j++){
+				if(generalList[j].toLowerCase() === wordArr[i].toLowerCase()){
+					generalCounter++;
+					ecScore += 10 / generalCounter;
+				} 
+			}
+		}
+	
+		return ecScore;
+
+
+}
+
+
+
+
+
+
+
+
 
